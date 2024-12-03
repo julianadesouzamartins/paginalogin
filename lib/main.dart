3fa1f8c7bb858e051f3fc51c1bc5c1da1b7edd65
josias13-dutra <62802901+josias13-dutra@users.noreply.github.com>
2024-12-03 18:30:27 -0400

    paginalogn

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:telas/home_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),));
+}
